e16128c00240872206fee0fc3b5ceda4e29993f5
Ian Douglas Scott <ian@iandouglasscott.com>
2016-12-18 11:37:51 -0800

    Make main.c read from file

--- main.c
+++ main.c
@@ -1,26 +1,46 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 #include "object.h"
 #include "tokenize.h"
 #include "parse.h"
 
+char *read_to_string(FILE *file) {
+    // FIXME: performance
+    char *text, next_char;
+    int len=0;
 
-int main() {
-    Object *object = new_list(NULL);
-    list_prepend(object, new_int(1));
-    list_prepend(object, new_double(7.2));
-    list_prepend(object, new_int(3));
-    list_prepend(object, new_string("test"));
-    object_print(object);
-
-    int ntoks;
-    char **toks = tokenize("(1.2 \"test\"  (1 2 3))", &ntoks);
-    printf("Tokens: %d\n", ntoks);
-    for (int i = 0; i < ntoks; i++) {
-        printf("%s\n", toks[i]);
+    text = malloc(1);
+    while ((next_char = fgetc(file)) != EOF) {
+        len++;
+        text = realloc(text, len+1);
+        text[len-1] = next_char;
     }
+    text[len] = '\0';
+
+    return text;
+}
+
+int main(int argc, char *argv[]) {
     int nobjects;
-    Object **objects = parse("(1.2 \"test\"  (1 2 3))", &nobjects);
+    Object **objects;
+    FILE *file;
+    char *text;
+
+    if (argc < 2) {
+        printf("Not enough arguments.\n");
+        exit(1);
+    }
+
+    file = fopen(argv[1], "r");
+    if (file == NULL) {
+        perror("Error");
+        exit(1);
+    }
+
+    text = read_to_string(file);
+
+    objects = parse(text, &nobjects);
     for (int i = 0; i < nobjects; i++) {
         object_print(objects[i]);
         printf("\n");
